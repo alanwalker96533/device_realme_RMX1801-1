b07de792b582519a0befb23a896d9ae78c07d51d
alanwalker96533 <94613115+alanwalker96533@users.noreply.github.com>
2022-09-19 19:42:23 +0530

    RMX1801 : spark bring up

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/arrow_RMX1801.mk
+    $(LOCAL_DIR)/spark_RMX1801.mk
 
 COMMON_LUNCH_CHOICES := \
-    arrow_RMX1801-user \
-    arrow_RMX1801-userdebug \
-    arrow_RMX1801-eng
+    spark_RMX1801-user \
+    spark_RMX1801-userdebug \
+    spark_RMX1801-eng
